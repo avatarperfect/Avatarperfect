076c9be57be9d9875b118e2bae0b7bd92446aee6
avatarperfect <kristinaaa.pa@gmail.com>
2025-01-12 01:57:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/questions', methods=['POST'])
-def questions():
-    return render_template('questions.html')
+@app.route("/upload", methods=["POST"])
+def upload():
+    if request.method == "POST":
+        photo = request.files["photo"]
+        voice = request.files["voice"]
+        video = request.files["video"]
 
-@app.route('/submit_answers', methods=['POST'])
-def submit_answers():
-    return "Спасибо! Аватар создан!"
+        # Сохраняем файлы
+        photo.save(os.path.join("static/uploads", photo.filename))
+        voice.save(os.path.join("static/uploads", voice.filename))
+        video.save(os.path.join("static/uploads", video.filename))
 
+        return redirect(url_for("success"))
+
+@app.route("/success")
+def success():
+    return "Данные успешно загружены! Создание аватара началось."
+
+# Привязываем приложение к динамическому порту Heroku
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
